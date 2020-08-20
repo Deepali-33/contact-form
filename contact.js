a8a9e49ee162c8de35407869c78bfb0e334fe620
Deepali-33 <deepali.tiwari030101@gmail.comgit config --global user.name Deepali-33>
2020-08-20 15:49:18 +0530

    update email and name block

--- contact.js
+++ contact.js
@@ -1,12 +1,29 @@
 
- function contactform() {
+function contactform() {
     console.log("click");
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
-    var email = document.getElementById("email").value;
+    var mail = document.getElementById("mail").value;
     var num  = document.getElementById("num").value;
     var msg = document.getElementById("msg").value;
     var mesg = document.getElementById("mesg").value;
+    buttonOK = "true";
 
-    alert("  Your name is "+fname+" "+lname+" , email is "+email+" , phone number is "+num+" message subject is "+msg+" message is "+mesg+"  Thank You. ");
-} 
+    if (num.length > 10 || num.length < 10) {
+      alert("Phone number should contain only 10 digits.");
+      buttonOK = "false";
+    } 
+
+  if (buttonOK == "false") {
+      return false;
+    } 
+
+    alert("  Your name is "+fname+" "+lname+" , Your email is "+mail+" , Your phone number is "+num+" , Your message subject is "+msg+" , Your message is "+mesg+" . </n> Thank You.")
+  }
+   
+
+  
+
+
+
+
